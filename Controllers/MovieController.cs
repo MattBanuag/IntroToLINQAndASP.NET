38d54fe32fd0cb7d4f4dbb817ad28d6275c8260a
MattBanuag <matthewbanuag.dev@gmail.com>
2023-02-09 16:51:00 -0600

    build: getAllInGenre action

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -17,11 +17,33 @@ namespace LAB_01.Controllers
             Movie movie = Context.Movies.First(m =>
             {
                 // Using 'Equals' to ignore case
-                return m.Title.Equals(title);
+                return m.Title.Equals(title, StringComparison.OrdinalIgnoreCase);
             }); 
 
             ViewBag.PageTitle = movie.Title;    
             return View("Details", movie);
+        }
+
+        public IActionResult GetAllInGenre(string genre)
+        {
+            List<string> genres = Enum.GetNames(typeof(Genres)).ToList();
+
+            if (genres.Contains(genre, StringComparer.OrdinalIgnoreCase))
+            {
+                HashSet<Movie> moviesInGenre = Context.Movies.Where(m =>
+                {
+                    return m.Genre.ToString().Equals(genre, StringComparison.OrdinalIgnoreCase);
+                }).ToHashSet();
+
+                ViewBag.MovieCount = moviesInGenre.Count;
+                ViewBag.PageTitle = $"Movies in {genre}";
+
+                return View("Index", moviesInGenre);
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
