b6b45f15fa5ffff9dabc2525789ca687ceb8e024
MattBanuag <matthewbanuag.dev@gmail.com>
2023-02-09 14:45:07 -0600

    build: actor model

--- Data/Context.cs
+++ Data/Context.cs
@@ -1,7 +1,28 @@
-﻿namespace LAB_01.Data
+﻿using LAB_01.Models;
+
+namespace LAB_01.Data
 {
     public static class Context
     {
+        public static HashSet<Movie> Movies = new HashSet<Movie>();
+        private static int _movieIdCounter = 0;
 
+        private static void _seedMethod()
+        {
+            Movie movie1 = new Movie(_movieIdCounter++, "Braveheart", new DateTime(2000, 5, 24), 65_000_000);
+            Movie movie2 = new Movie(_movieIdCounter++, "Forrest Gump", new DateTime(1994, 7, 6), 55_000_000);
+            Movie movie3 = new Movie(_movieIdCounter++, "Taken", new DateTime(2009, 1, 30), 25_000_000);
+            Movie movie4 = new Movie(_movieIdCounter++, "The Goonies", new DateTime(1985, 6, 9), 19_000_000);
+
+            Movies.Add(movie1);
+            Movies.Add(movie2);
+            Movies.Add(movie3);
+            Movies.Add(movie4);
+        }
+
+        static Context()
+        {
+            _seedMethod();
+        }
     }
 }
--- Models/Actor.cs
+++ Models/Actor.cs
@@ -4,5 +4,65 @@ namespace LAB_01.Models
 {
     public class Actor
     {
+        private readonly int _id;
+        public int Id { get { return _id; } }
+
+        private string _name;
+        public string Name 
+        { 
+            get { return _actorName; } 
+            set
+            {
+                if(value.Length > 0)
+                {
+                    _actorName = value;
+                } else
+                {
+                    throw new Exception("Actor's name cannot have a length of zero.");
+                }
+            }
+        
+        }
+
+        private int _salary;
+        public int Salary
+        {
+            get { return _salary; } 
+            set 
+            {
+                if(value > 0) 
+                {
+                    _salary = value;
+                } else
+                {
+                    throw new Exception("Actor's salary cannot be zero.");
+                }
+            }
+        }
+
+        private HashSet<Movie> _movies = new HashSet<Movie>();
+
+        // === METHODS
+        public HashSet<Movie> GetMoviesOfActor()
+        {
+            return _movies.ToHashSet();
+        }
+        public void AddMovieForActor(Movie movie)
+        {
+            _movies.Add(movie);
+        }
+
+        // === CONSTRUCTORS
+        public Actor()
+        {
+
+        }
+
+        public Actor(int id, string name, int salary)
+        {
+            _id = id;
+            Name = name;
+            Salary = salary;
+        }
     }
 }
