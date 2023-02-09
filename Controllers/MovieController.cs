e1826140606015dc9afcde836a7b1b04eed7b2a9
MattBanuag <matthewbanuag.dev@gmail.com>
2023-02-09 15:55:08 -0600

    build: movie controller

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LAB_01.Data;
+using LAB_01.Models;
+
+namespace LAB_01.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.PageTitle = "All movies";
+            return View(Context.Movies);
+        }
+
+
+    }
+}
