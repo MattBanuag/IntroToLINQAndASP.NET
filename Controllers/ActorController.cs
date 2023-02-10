e2dd0675c6a6e6318529f47e686a108b65229fea
MattBanuag <matthewbanuag.dev@gmail.com>
2023-02-09 18:30:00 -0600

    build: actor controller

---
+++ Controllers/ActorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LAB_01.Controllers
+{
+    public class ActorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
