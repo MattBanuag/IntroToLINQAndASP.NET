5b460ed718fb4f1fa8b7f03cb92f690da212d0b4
MattBanuag <matthewbanuag.dev@gmail.com>
2023-02-10 17:17:21 -0600

    build: rating controller, create rating view, and rate movie view model

---
+++ Controllers/RatingController.cs
@@ -0,0 +1,41 @@
+﻿using LAB_01.Data;
+using LAB_01.Models;
+using LAB_01.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LAB_01.Controllers
+{
+    public class RatingController : Controller
+    {
+        public IActionResult Create()
+        {
+            RateMovieVM vm = new RateMovieVM(Context.Movies, Context.Users);
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("MovieId", "UserId", "Score", "Comment")] RateMovieVM vm)
+        {
+            try
+            {
+                Movie movie = Context.Movies.First(m => m.Id == Int32.Parse(vm.MovieId));
+                User user = Context.Users.First(u => u.Id == Int32.Parse(vm.UserId));
+                int score = vm.Score;
+                string comment = vm.Comment;
+
+                // Create new rating and add to Context/Actor relationships
+                Rating newRating = new Rating(Context.RatingIdCounter++, score, user, movie, comment);
+
+                movie.AddRating(newRating);
+                user.AddRating(newRating);
+                Context.Ratings.Add(newRating);
+
+                return RedirectToAction("Info", "Movie", new { id = movie.Id });
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Models/ViewModels/RateMovieVM.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LAB_01.Models.ViewModels
+{
+    public class RateMovieVM
+    {
+        public List<SelectListItem> Movies { get; } = new List<SelectListItem>();
+        public List<SelectListItem> Users { get; } = new List<SelectListItem>();
+        public string MovieId { get; set; }
+        public string UserId { get; set; }
+        public int Score { get; set; }
+        public string Comment { get; set; }
+
+        public RateMovieVM(HashSet<Movie> movies, HashSet<User> users)
+        {
+            foreach (Movie m in movies)
+            {
+                Movies.Add(new SelectListItem(m.Title, m.Id.ToString()));
+            }
+
+            foreach (User u in users)
+            {
+                Users.Add(new SelectListItem(u.Name, u.Id.ToString()));
+            }
+        }
+
+        public RateMovieVM()
+        {
+
+        }
+    }
+}
