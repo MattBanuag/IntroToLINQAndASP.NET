96bf9fcbb91bec8e599d6559a8671fc00bc7cd11
MattBanuag <matthewbanuag.dev@gmail.com>
2023-02-12 11:48:45 -0600

    build: compareMovies ViewModel

---
+++ Models/ViewModels/CompareMoviesVM.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LAB_01.Models.ViewModels
+{
+    public class CompareMoviesVM
+    {
+        public List<SelectListItem> FirstListOfMovies { get; } = new List<SelectListItem>();
+        public List<SelectListItem> SecondListOfMovies { get; } = new List<SelectListItem>();
+        public int FirstMovieId { get; set; }
+        public int SecondMovieId { get; set; }
+
+        public CompareMoviesVM(HashSet<Movie> firstListOfMovies, HashSet<Movie> secondListOfMovies)
+        {
+            foreach (Movie m in firstListOfMovies)
+            {
+                FirstListOfMovies.Add(new SelectListItem(m.Title, m.Id.ToString()));
+            }
+
+            foreach (Movie m in secondListOfMovies)
+            {
+                SecondListOfMovies.Add(new SelectListItem(m.Title, m.Id.ToString()));
+            }
+        }
+
+        public CompareMoviesVM()
+        {
+
+        }
+    }
+}
